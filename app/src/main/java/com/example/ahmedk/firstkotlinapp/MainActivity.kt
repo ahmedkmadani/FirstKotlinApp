5ea33a097aa6515c2e7a210b9fe5fa16ea443a8a
[ahmedkmadani] <[ahmedkamal543@gmail.com]>
2019-01-28 01:57:45 +0000

    add fucntion to add item to listView and preview listview in layout

--- app/src/main/java/com/example/ahmedk/firstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/ahmedk/firstkotlinapp/MainActivity.kt
@@ -3,19 +3,46 @@ package com.example.ahmedk.firstkotlinapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
+
+    var listItems = ArrayList<String>()
+    var adapter: ArrayAdapter<String>? = null
+    var i: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //adding floating button to activity
+        //declaring vars
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
+        listView.adapter = adapter
+
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+
+            //function to add item into listView
+            i= i+1
+            addListItem()
+            Snackbar.make(view, "You just Pressed floating button, event saved in listView", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+
         }
+
+
+    }
+
+    fun addListItem(){
+
+        //adding items into listView
+        listItems.add("Saving Click Event into listView No: " +i )
+        adapter?.notifyDataSetChanged()
 
     }
 }
